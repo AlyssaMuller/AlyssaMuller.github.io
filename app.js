7d622c30147ff9cbf59609961c1c6ff64b0d0b25
Alyssa <alyssanm18@gmail.com>
2022-04-11 23:22:03 -0600

    authentication

--- app.js
+++ app.js
@@ -212,113 +212,117 @@ function deleteChocolateFromServer(chocolateID) {
 function loadChocolates() {
     fetch("http://localhost:8080/chocolates", { credentials: 'include' }).then(function (response) {
         if (response.status == 401) {
-
             unauthUI.style.display = "block";   //show login/register UI
             chocolatesUI.style.display = "none"; //hide List
             registerUI.style.display = "none"; //hide register
             loginUI.style.display = "none"; //hide login
             return;
-        }
-        // The server has responded.
-        response.json().then(function (data) {
-            serverChocolates = data //this is the list
-            console.log("Chocolates from the server:", serverChocolates);
+        } else if (response.status == 200) {
+            unauthUI.style.display = "none";   //show login/register UI
+            chocolatesUI.style.display = "grid"; //hide List
+            registerUI.style.display = "none"; //hide register
+            loginUI.style.display = "none"; //hide login
+            // The server has responded.
+            response.json().then(function (data) {
+                serverChocolates = data //this is the list
+                console.log("Chocolates from the server:", serverChocolates);
 
-            //step 1:query the PARENT element
-            var chocolateList = document.querySelector("#chocolate-list");
-            console.log("chocolate list query: ", chocolateList);
+                //step 1:query the PARENT element
+                var chocolateList = document.querySelector("#chocolate-list");
+                console.log("chocolate list query: ", chocolateList);
 
-            //empty the list of chocolates
-            chocolateList.innerHTML = "";
+                //empty the list of chocolates
+                chocolateList.innerHTML = "";
 
-            //loop over data immediately
-            //for chocolates in faveChocolates, append each to the DOM list
-            serverChocolates.forEach(function (chocolate) {
-                //see DOM insert/append code steps
-                //step 2: create the child element
+                //loop over data immediately
+                //for chocolates in faveChocolates, append each to the DOM list
+                serverChocolates.forEach(function (chocolate) {
+                    //see DOM insert/append code steps
+                    //step 2: create the child element
 
-                var newListItem = document.createElement("div");
-                newListItem.classList.add("outputDivs");
+                    var newListItem = document.createElement("div");
+                    newListItem.classList.add("outputDivs");
 
-                var nameDiv = document.createElement("div");
-                // Combining the name and flavor into one
-                nameDiv.innerHTML = chocolate.name + " " + chocolate.flavor;
-                nameDiv.classList.add("chocolate-name");
-                newListItem.appendChild(nameDiv);
+                    var nameDiv = document.createElement("div");
+                    // Combining the name and flavor into one
+                    nameDiv.innerHTML = chocolate.name + " " + chocolate.flavor;
+                    nameDiv.classList.add("chocolate-name");
+                    newListItem.appendChild(nameDiv);
 
-                /* var flavorDiv = document.createElement("div"); flavorDiv.innerHTML = chocolate.flavor; flavorDiv.classList.add("chocolate-flavor"); newListItem.appendChild(flavorDiv); */
+                    /* var flavorDiv = document.createElement("div"); flavorDiv.innerHTML = chocolate.flavor; flavorDiv.classList.add("chocolate-flavor"); newListItem.appendChild(flavorDiv); */
 
-                var priceDiv = document.createElement("div");
-                priceDiv.innerHTML = "ONLY " + chocolate.price + "!";
-                priceDiv.classList.add("chocolate-price");
-                newListItem.appendChild(priceDiv);
+                    var priceDiv = document.createElement("div");
+                    priceDiv.innerHTML = "ONLY " + chocolate.price + "!";
+                    priceDiv.classList.add("chocolate-price");
+                    newListItem.appendChild(priceDiv);
 
-                var sizeDiv = document.createElement("div");
-                sizeDiv.innerHTML = "Size: " + chocolate.size;
-                sizeDiv.classList.add("chocolate-size");
-                newListItem.appendChild(sizeDiv);
+                    var sizeDiv = document.createElement("div");
+                    sizeDiv.innerHTML = "Size: " + chocolate.size;
+                    sizeDiv.classList.add("chocolate-size");
+                    newListItem.appendChild(sizeDiv);
 
-                var descriptionDiv = document.createElement("div");
-                descriptionDiv.innerHTML = chocolate.description;
-                descriptionDiv.classList.add("chocolate-description");
-                newListItem.appendChild(descriptionDiv);
+                    var descriptionDiv = document.createElement("div");
+                    descriptionDiv.innerHTML = chocolate.description;
+                    descriptionDiv.classList.add("chocolate-description");
+                    newListItem.appendChild(descriptionDiv);
 
-                var ratingDiv = document.createElement("div");
-                ratingDiv.innerHTML = "";
+                    var ratingDiv = document.createElement("div");
+                    ratingDiv.innerHTML = "";
 
-                if (chocolate.rating == 5)
-                    ratingDiv.innerHTML += "⭐⭐⭐⭐⭐";
-                else {
-                    for (var i = 0; i < chocolate.rating; i++)
-                        ratingDiv.innerHTML += "⭐";
+                    if (chocolate.rating == 5)
+                        ratingDiv.innerHTML += "⭐⭐⭐⭐⭐";
+                    else {
+                        for (var i = 0; i < chocolate.rating; i++)
+                            ratingDiv.innerHTML += "⭐";
 
-                    for (var j = chocolate.rating; j < 5; j++)
-                        ratingDiv.innerHTML += "★";
-                }
-
-                ratingDiv.classList.add("chocolate-rating");
-                newListItem.appendChild(ratingDiv);
-
-                var editButton = document.createElement("button");
-                editButton.innerHTML = "Edit";
-                editButton.classList.add("editButton");
-                editButton.onclick = function () {
-                    var chocolateNameInput = document.querySelector("#chocolateName");
-                    var chocolateFlavorInput = document.querySelector("#chocolateFlavor");
-                    var chocolatePriceInput = document.querySelector("#chocolatePrice");
-                    var chocolateSizeInput = document.querySelector("#chocolateSize");
-                    var chocolateDescriptionInput = document.querySelector("#chocolateDescription");
-                    var chocolateRatingInput = document.querySelector("#chocolateRating");
-
-                    chocolateNameInput.value = chocolate.name;
-                    chocolateFlavorInput.value = chocolate.flavor;
-                    chocolatePriceInput.value = chocolate.price;
-                    chocolateSizeInput.value = chocolate.size;
-                    chocolateDescriptionInput.value = chocolate.description;
-                    chocolateRatingInput.value = chocolate.rating;
-
-                    adding = false;
-                    chocolateID = chocolate.id;
-                    addButton.innerHTML = "Save Chocolate";
-                };
-
-                newListItem.appendChild(editButton);
-
-                var deleteButton = document.createElement("button");
-                deleteButton.innerHTML = "Delete";
-                deleteButton.classList.add("deleteButton");
-                deleteButton.onclick = function () {
-                    console.log("delete button clicked, id: ", chocolate.id);
-                    if (confirm("Are you sure?")) {
-                        deleteChocolateFromServer(chocolate.id);
+                        for (var j = chocolate.rating; j < 5; j++)
+                            ratingDiv.innerHTML += "★";
                     }
-                };
-                newListItem.appendChild(deleteButton);
 
-                // step 3: append child to parent
-                chocolateList.appendChild(newListItem);
+                    ratingDiv.classList.add("chocolate-rating");
+                    newListItem.appendChild(ratingDiv);
+
+                    var editButton = document.createElement("button");
+                    editButton.innerHTML = "Edit";
+                    editButton.classList.add("editButton");
+                    editButton.onclick = function () {
+                        var chocolateNameInput = document.querySelector("#chocolateName");
+                        var chocolateFlavorInput = document.querySelector("#chocolateFlavor");
+                        var chocolatePriceInput = document.querySelector("#chocolatePrice");
+                        var chocolateSizeInput = document.querySelector("#chocolateSize");
+                        var chocolateDescriptionInput = document.querySelector("#chocolateDescription");
+                        var chocolateRatingInput = document.querySelector("#chocolateRating");
+
+                        chocolateNameInput.value = chocolate.name;
+                        chocolateFlavorInput.value = chocolate.flavor;
+                        chocolatePriceInput.value = chocolate.price;
+                        chocolateSizeInput.value = chocolate.size;
+                        chocolateDescriptionInput.value = chocolate.description;
+                        chocolateRatingInput.value = chocolate.rating;
+
+                        adding = false;
+                        chocolateID = chocolate.id;
+                        addButton.innerHTML = "Save Chocolate";
+                    };
+
+                    newListItem.appendChild(editButton);
+
+                    var deleteButton = document.createElement("button");
+                    deleteButton.innerHTML = "Delete";
+                    deleteButton.classList.add("deleteButton");
+                    deleteButton.onclick = function () {
+                        console.log("delete button clicked, id: ", chocolate.id);
+                        if (confirm("Are you sure?")) {
+                            deleteChocolateFromServer(chocolate.id);
+                        }
+                    };
+                    newListItem.appendChild(deleteButton);
+
+                    // step 3: append child to parent
+                    chocolateList.appendChild(newListItem);
+                });
             });
-        });
+        }
     });
 }
 
